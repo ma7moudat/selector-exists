009ead3ee79ddc2abc180d7422bdac80c042bb69
Mahmoud Aldaas <mahmoud@patrick-and-friends.de>
2020-12-07 12:21:18 +0100

    additional tests, finalise package.json with publishing scripts

--- src/__tests__/SelectorExists.test.ts
+++ src/__tests__/SelectorExists.test.ts
@@ -1,6 +1,30 @@
-import {SelectorExists} from '../SelectorExists';
+import {ReaderStdin, SelectorExists} from '../index';
 
 test('No Usages', () => {
   const instance = new SelectorExists();
   expect(instance.getUsages().length).toBe(0);
+});
+
+test('STDIN', async () => {
+  const instance = new SelectorExists();
+  instance.addCssSource(new ReaderStdin(`
+    body {
+      background-color: #fff;
+    }
+    div.block {
+      font-size: 12px; 
+    }
+    .missing {
+      border: 1px solid red;
+    }
+  `));
+  instance.addHtmlSource(new ReaderStdin(`
+    <html lang="en">
+        <body>
+            <div class="block">Demo</div>
+        </body>
+    </html>
+  `));
+  await instance.processUsages();
+  expect(instance.getUsages().length).toBe(3);
 });
